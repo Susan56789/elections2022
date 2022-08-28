6b9137baffe48ba8a3b3eb7b44f9e0ec351cc32e
Susan Wairimu <mwihaki2susan@gmail.com>
2022-08-28 07:25:05 +0300

    data

---
+++ src/Components/Data/Counties.js
@@ -0,0 +1,111 @@
+export const county = [
+  {
+    id: 1,
+    name: "Mombasa",
+  },
+  {
+    id: 2,
+    name: "Kwale",
+  },
+  {
+    id: 3,
+    name: "Kilifi",
+  },
+  {
+    id: 4,
+    name: "Tana River",
+  },
+  {
+    id: 5,
+    name: "Lamu",
+  },
+  {
+    id: 6,
+    name: "Taita Taveta",
+  },
+  {
+    id: 7,
+    name: "Garissa",
+  },
+  {
+    id: 8,
+    name: "Wajir",
+  },
+  {
+    id: 9,
+    name: "Mandera",
+  },
+  {
+    id: 10,
+    name: "Marsabit",
+  },
+  {
+    id: 11,
+    name: "Isiolo",
+  },
+  {
+    id: 12,
+    name: "Meru",
+  },
+  {
+    id: 13,
+    name: "Tharaka Nithi",
+  },
+  {
+    id: 14,
+    name: "Embu",
+  },
+  {
+    id: 15,
+    name: "Kitui",
+  },
+  {
+    id: 16,
+    name: "Machakos",
+  },
+  {
+    id: 17,
+
+    name: "Makueni",
+  },
+  {
+    id: 18,
+    name: "Nyandarua",
+  },
+  {
+    id: 19,
+    name: "Nyeri",
+  },
+  {
+    id: 20,
+    name: "Kirinyaga",
+  },
+  {
+    id: 21,
+    name: "Murang'a",
+  },
+  {
+    id: 22,
+    name: "Kiambu",
+  },
+  {
+    id: 23,
+    name: "Turkana",
+  },
+  {
+    id: 24,
+    name: "West Pokot",
+  },
+  {
+    id: 25,
+    name: "Samburu",
+  },
+  {
+    id: 26,
+    name: "Trans-Nzoia",
+  },
+  {
+    id: 27,
+    name: "Uasin Gishu",
+  },
+];
