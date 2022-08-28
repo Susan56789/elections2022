cf046be5de7dad578081bbbfaee2f29ad1545f3d
Susan Wairimu <mwihaki2susan@gmail.com>
2022-08-28 12:52:04 +0300

    results

---
+++ src/Components/Data/Senator.js
@@ -0,0 +1,332 @@
+export const senator = [
+  {
+    county: "Mombasa",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kwale",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+    candidate2: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+    candidate3: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kilifi",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Tana River",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Lamu",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Taita Taveta",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Garissa",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Wajir",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Mandera",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Marsabit",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Isiolo",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Meru",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Tharaka Nithi",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Embu",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kitui",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Machakos",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Makueni",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Nyandarua",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Nyeri",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kirinyaga",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kiambu",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kisii",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Nakuru",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Turkana",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Uasin Gishu",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Elgeyo Marakwet",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Nandi",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Baringo",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Laikipia",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kakamega",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Vihiga",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Bungoma",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kajiado",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kakamega",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kisumu",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Homa Bay",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Migori",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Kisii",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Nyamira",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+  {
+    county: "Nairobi",
+    candidate1: {
+      name: "",
+      party: "",
+      votes: 0,
+    },
+  },
+];
