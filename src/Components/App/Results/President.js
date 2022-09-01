163041d3bf12f24dfdcbe08a3acda12a8f467ad4
Susan Wairimu <mwihaki2susan@gmail.com>
2022-09-01 15:17:21 +0300

    h1 element

--- src/Components/App/Results/President.js
+++ src/Components/App/Results/President.js
@@ -4,42 +4,45 @@ import "./cards.css";
 
 export default function President() {
   return (
-    <div className="cards">
-      {president.map((president) => (
-        <>
-          <div className="card">
-            <caption>{president.county} County</caption>
+    <>
+      <h1>Presidential Results Per County</h1>
+      <div className="cards">
+        {president.map((president) => (
+          <>
+            <div className="card">
+              <caption>{president.county} County</caption>
 
-            <tbody>
-              <tr>
-                <td>Candidate Names</td>
-                <td>Party</td>
-                <td>Votes</td>
-              </tr>
-              <tr>
-                <td> {president.candidate1.name}</td>
-                <td> {president.candidate1.party}</td>
-                <td> {president.candidate1.votes}</td>
-              </tr>
-              <tr>
-                <td> {president.candidate2.name}</td>
-                <td> {president.candidate2.party}</td>
-                <td> {president.candidate2.votes}</td>
-              </tr>
-              <tr>
-                <td> {president.candidate3.name}</td>
-                <td> {president.candidate3.party}</td>
-                <td> {president.candidate3.votes}</td>
-              </tr>
-              <tr>
-                <td> {president.candidate4.name}</td>
-                <td> {president.candidate4.party}</td>
-                <td> {president.candidate4.votes}</td>
-              </tr>
-            </tbody>
-          </div>
-        </>
-      ))}
-    </div>
+              <tbody>
+                <tr>
+                  <td>Candidate Names</td>
+                  <td>Party</td>
+                  <td>Votes</td>
+                </tr>
+                <tr>
+                  <td> {president.candidate1.name}</td>
+                  <td> {president.candidate1.party}</td>
+                  <td> {president.candidate1.votes}</td>
+                </tr>
+                <tr>
+                  <td> {president.candidate2.name}</td>
+                  <td> {president.candidate2.party}</td>
+                  <td> {president.candidate2.votes}</td>
+                </tr>
+                <tr>
+                  <td> {president.candidate3.name}</td>
+                  <td> {president.candidate3.party}</td>
+                  <td> {president.candidate3.votes}</td>
+                </tr>
+                <tr>
+                  <td> {president.candidate4.name}</td>
+                  <td> {president.candidate4.party}</td>
+                  <td> {president.candidate4.votes}</td>
+                </tr>
+              </tbody>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
   );
 }
